a13837169fd2ef8fccfb96964655a37aafe20653
Gavin Ryan <grya8915@uni.sydney.edu.au>
2024-08-13 00:30:38 +1000

    Added the layouts for main and edit. Also added checkbox list

---
+++ app/src/main/java/com/example/todo/CustomAdapter.java
@@ -0,0 +1,80 @@
+package com.example.todo;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends ArrayAdapter<DataModel> {
+
+    private ArrayList<DataModel> dataSet;
+    private Context mContext;
+    private OnCheckboxClickListener onCheckboxClickListener;
+
+    // Interface for checkbox click listener
+    public interface OnCheckboxClickListener {
+        void onCheckboxClick(int position);
+    }
+    
+    public CustomAdapter(ArrayList<DataModel> dataSet, Context mContext) {
+        super(mContext, R.layout.raw_item, dataSet);
+        this.dataSet = dataSet;
+        this.mContext = mContext;
+    }
+
+    // Setter for the listener
+    public void setOnCheckboxClickListener(OnCheckboxClickListener listener) {
+        this.onCheckboxClickListener = listener;
+    }
+
+    private static class ViewHolder {
+        TextView txtName;
+        CheckBox checkBox;
+    }
+
+    @Override
+    public int getCount() {
+        return dataSet.size();
+    }
+
+    @Override
+    public DataModel getItem(int position) {
+        return dataSet.get(position);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+        View result;
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.raw_item, parent, false);
+            viewHolder.txtName = convertView.findViewById(R.id.txtName);
+            viewHolder.checkBox = convertView.findViewById(R.id.checkBox);
+            result = convertView;
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+            result = convertView;
+        }
+        DataModel dataModel = getItem(position);
+        assert dataModel != null;
+        viewHolder.txtName.setText(dataModel.getName());
+        viewHolder.checkBox.setChecked(dataModel.isChecked());
+
+        viewHolder.checkBox.setOnClickListener(v -> {
+            if (onCheckboxClickListener != null) {
+                onCheckboxClickListener.onCheckboxClick(position);
+            }
+        });
+        return result;
+    }
+}
--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -1,33 +1,26 @@
 package com.example.todo;
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import java.text.BreakIterator;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     // Define variables
     ListView listView;
     ArrayList<String> items;
-    ArrayAdapter<String> itemsAdapter;
+    CustomAdapter adapter;
     EditText addItemEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,18 +28,15 @@ public class MainActivity extends AppCompatActivity {
 // Use "activity_main.xml" as the layout
         setContentView(R.layout.activity_main);
 // Reference the "listView" variable to the id "lstView" in the layout
-        listView = (ListView) findViewById(R.id.lstView);
-        addItemEditText = (EditText) findViewById(R.id.txtNewItem);
-// Create an ArrayList of String
-        items = new ArrayList<>();
-        items.add("item one");
-        items.add("item two");
-// Create an adapter for the list view using Android's built-in item layout
-        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
-                items);
-// Connect the listView and the adapter
-        listView.setAdapter(itemsAdapter);
+        listView = findViewById(R.id.lstView);
 
+// Create an adapter for the list view using Android's built-in item layout
+        ArrayList<DataModel> dataModels = new ArrayList<>();
+        dataModels.add(new DataModel("item one", false));
+        dataModels.add(new DataModel("item two", false));
+        adapter = new CustomAdapter(dataModels, this);
+
+        listView.setAdapter(adapter);
         // Setup listView listeners
         setupListViewListener();
     }
@@ -56,67 +46,49 @@ public class MainActivity extends AppCompatActivity {
             result -> {
                 if (result.getResultCode() == RESULT_OK) {
 // Extract name value from result extras
-                    String editedItem = result.getData().getExtras().getString("item");
+                    assert result.getData() != null;
+                    String editedItem = Objects.requireNonNull(result.getData().getExtras()).getString("item");
                     int position = result.getData().getIntExtra("position", -1);
                     items.set(position, editedItem);
                     Log.i("Updated item in list ", editedItem + ", position: " + position);
 // Make a standard toast that just contains text
                     Toast.makeText(getApplicationContext(), "Updated: " + editedItem,
                             Toast.LENGTH_SHORT).show();
-                    itemsAdapter.notifyDataSetChanged();
+                    adapter.notifyDataSetChanged();
                 }
             }
     );
 
     private void setupListViewListener() {
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            public boolean onItemLongClick(AdapterView<?> parent, View view, final int
-                    position, long rowId)
-            {
-                Log.i("MainActivity", "Long Clicked item " + position);
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle(R.string.dialog_delete_title)
-                        .setMessage(R.string.dialog_delete_msg)
-                        .setPositiveButton(R.string.delete, new
-                                DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialogInterface, int i) {
-                                        items.remove(position); // Remove item from the ArrayList
-                                        itemsAdapter.notifyDataSetChanged(); // Notify listView adapter to update the list
-                                    }
-                                })
-                        .setNegativeButton(R.string.cancel, new
-                                DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialogInterface, int i) {
-                                        // User cancelled the dialog
-                                        // Nothing happens
-                                    }
-                                });
-                builder.create().show();
-                return true;
-            }
+        listView.setOnItemLongClickListener((parent, view, position, rowId) -> {
+            Log.i("MainActivity", "Long Clicked item " + position);
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle(R.string.dialog_delete_title)
+                    .setMessage(R.string.dialog_delete_msg)
+                    .setPositiveButton(R.string.delete, (dialogInterface, i) -> {
+                        items.remove(position); // Remove item from the ArrayList
+                        adapter.notifyDataSetChanged(); // Notify listView adapter to update the list
+                    })
+                    .setNegativeButton(R.string.cancel, (dialogInterface, i) -> {
+                        // User cancelled the dialog
+                        // Nothing happens
+                    });
+            builder.create().show();
+            return true;
         });
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String updateItem = (String) itemsAdapter.getItem(position);
-                Log.i("MainActivity", "Clicked item " + position + ": " + updateItem);
-                Intent intent = new Intent(MainActivity.this, EditToDoItemActivity.class);
-                if (intent != null) {
-                // put "extras" into the bundle for access in the edit activity
-                    intent.putExtra("item", updateItem);
-                    intent.putExtra("position", position);
-                // brings up the second activity
-                    mLauncher.launch(intent);
-                    itemsAdapter.notifyDataSetChanged();
-                }
-            }
-        });
+        // Set a click listener on the checkbox within each list item
+        adapter.setOnCheckboxClickListener(position -> {
+            DataModel dataModel = adapter.getItem(position);
+            assert dataModel != null;
+            dataModel.setChecked(!dataModel.isChecked());
+            adapter.notifyDataSetChanged();});
     }
 
     public void onAddItemClick(View view) {
         String toAddString = addItemEditText.getText().toString();
-        if (toAddString != null && toAddString.length() > 0) {
-            itemsAdapter.add(toAddString); // Add text to list view adapter
+        if (!toAddString.isEmpty()) {
+//            adapter.add(toAddString); // Add text to list view adapter
+            adapter.add(new DataModel(toAddString, false));
             addItemEditText.setText("");
         }
     }
